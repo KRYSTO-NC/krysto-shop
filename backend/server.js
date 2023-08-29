97b9b4a150ad6156b438cea77dbf6747961d91e0
Stoyann Velten <stoyann.openclassrooms@gmail.com>
2023-08-29 15:30:29 +1100

    User email and password validation for login user

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,16 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
+
 const port = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("API is running...");
